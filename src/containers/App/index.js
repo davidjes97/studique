4f73c7c585c8edc3157686cc651cb6363a635dbf
Jesse David <davidjes@mail.gvsu.edu>
2019-11-13 16:00:26 -0500

    Its just doing things

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,8 +1,36 @@
 import React, { Component } from 'react';
 import * as firebase from "firebase";
+
 import config from './firebase-config';
 
 class App extends Component{
+    constructor(){
+        super();
+
+        //Initialize Firebase
+        firebase.initializeApp(config);
+    }
+
+    // state = {
+    //     posts: [],
+    //     loading: false
+    // };
+
+    componentWillMount(){
+        let postsRef = firebase.database().ref('posts');
+
+        let _this = this;
+
+        postsRef.on('value', function(snapshot) {
+            console.log(snapshot.val());
+
+            _this.setState({
+                posts: snapshot.val(),
+                loading: false
+            });
+        });
+    }
+
     render(){
         return (
             <div className="App">
@@ -13,28 +41,7 @@ class App extends Component{
             })}
           </div>
         );
-    }
-    constructor(){
-        super();
-
-        //Initialize Firebase
-        firebase.initializeApp(config);
-    }
-
-    componentWillMount(){
-        let postsRef = firebase.database().ref('posts');
-
-        let _this = this;
-
-        postsRef.on('value', function(snapshot){
-            console.log(snapshot.val());
-
-            _this.setState({
-                posts: snapshot.val(),
-                loading: false
-            });
-        });
-    }
+    }  
 }
 
 export default App;
