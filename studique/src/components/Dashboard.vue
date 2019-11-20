5cd1bfe2dc57d5b6e564968894aa08185ee69265
Jesse David <davidjes@mail.gvsu.edu>
2019-11-20 17:56:39 -0500

    Added in comments....and alot of other stuff
    Just need to make some javascript too do this with real data.

--- studique/src/components/Dashboard.vue
+++ studique/src/components/Dashboard.vue
@@ -6,31 +6,58 @@
         <div id="askForm">
           <h3 id="label" for="question">Question:</h3>
           <p></p>
-          <textarea v-model="text" rows="0" max-rows="4" id="question"></textarea>
+          <v-container fluid>
+            <v-textarea clearable clear-icon="x" auto-grow rounded outlined rows="1"></v-textarea>
+          </v-container>
 
           <v-btn color="primary" @click="buttonHandler" id="askButton">Ask</v-btn>
         </div>
         <div id="cards">
-            <v-list-item>
-                <v-card id="questionCard" class="mx-auto"
-                max-width="1000px"
-                outlined
-                :elevation="3"
-                >
-                <div id ="username" class="overline mb-4">USER's NAME</div>
-                <v-card-text>
-                <div class="text--primary">
-                    How do these questions work? Just writing text to try and fill the page</div>
-                </v-card-text>
-                </v-card>
-            </v-list-item>
+          <v-list-item>
+            <v-card id="questionCard" class="mx-auto" max-width="1000px" outlined :elevation="3">
+              <div id="username" class="overline mb-4">USER's NAME</div>
+              <v-card-text>
+                <div
+                  class="text--primary"
+                >How do these questions work? Just writing text to try and fill the page</div>
+              </v-card-text>
+              <v-expansion-panels>
+                <v-expansion-panel>
+                  <v-expansion-panel-header>Responses</v-expansion-panel-header>
+                  <v-expansion-panel-content>
+                    <v-card>
+                      <v-card-text>This is my comment</v-card-text>
+                    </v-card>
+                    <v-card>
+                      <v-card-text>Don't mind me just commenting on this very very very interesting question</v-card-text>
+                    </v-card>
+                  </v-expansion-panel-content>
+                </v-expansion-panel>
+              </v-expansion-panels>
+              <v-card-actions>
+                <v-container fluid>
+                  <v-textarea
+                    id="commentInput"
+                    label="Answer Question"
+                    clearable
+                    clear-icon="x"
+                    auto-grow
+                    rounded
+                    outlined
+                    rows="1"
+                    dense
+                    hide-details
+                  ></v-textarea>
+                </v-container>
+              </v-card-actions>
+            </v-card>
+          </v-list-item>
         </div>
       </div>
       <div id="emptyColumn"></div>
     </div>
   </div>
 </template>
-
 <script>
 export default {
   // data: function(){
@@ -40,14 +67,20 @@ export default {
 </script>
 
 <style scoped>
+#label {
+  padding: 5px;
+}
+#commentCard {
+  width: 100%;
+}
+
 #questionCard {
-    margin: 5px;
-    padding: 5px;
-    width: 98%;
+  margin-top: 5px;
+  width: 98%;
 }
 
 #username {
-    padding: 5px;
+  padding: 5px;
 }
 
 #dashboard {
@@ -60,7 +93,6 @@ export default {
   grid-template-columns: 4fr 1fr;
   grid-template-rows: 1fr auto;
   border-bottom: 2px solid gray;
-  padding: 5px;
 }
 
 #tagColumn {
@@ -79,5 +111,27 @@ export default {
 
 #askButton {
   margin: 5px;
+  margin-top: 20px;
+}
+
+@media only screen and (max-width: 1026px) {
+  #emptyColumn {
+    display: none;
+  }
+  #tagColumn {
+    display: none;
+  }
+
+  #askForm {
+    display: grid;
+    grid-template-columns: 4fr 1fr;
+    grid-template-rows: 1fr auto;
+    border-bottom: 2px solid gray;
+    padding: 5px;
+  }
+  #dashboard {
+    display: grid;
+    grid-template-columns: auto;
+  }
 }
 </style>
