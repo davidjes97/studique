bf85ed8467e4f57161267a5c58e56175389a4a04
Jesse David <46247412+davidjes97@users.noreply.github.com>
2019-11-21 12:59:14 -0500

    reverse order of cards to most recent up top

--- studique/src/components/Dashboard.vue
+++ studique/src/components/Dashboard.vue
@@ -21,7 +21,7 @@
         </div>
         <v-divider></v-divider>
         <div id="cards">
-          <v-list-item v-for="(myQuestion,pos) in myQuestion" :key="pos">
+          <v-list-item v-for="(myQuestion,pos) in myQuestion.slice().reverse()" :key="pos">
             <v-card class="questionCard" max-width="1000px" outlined :elevation="3">
               <v-card-subtitle id="username">USER's NAME</v-card-subtitle>
               <v-card-text>
