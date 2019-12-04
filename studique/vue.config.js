6873f743e292fbc6c716bbc2df3a0afab0940903
Jesse David <davidjes@mail.gvsu.edu>
2019-12-04 15:18:07 -0500

    Built

--- studique/vue.config.js
+++ studique/vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
+  
+    publicPath:
+    process.env.NODE_ENV === "production"
+    ? "https://davidjes97.github.io/studique/"
+    : "/"
+,
+  
+  
   "transpileDependencies": [
     "vuetify"
   ]
-}
+};
